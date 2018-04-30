ca2a61e9f9e363aeef58e1ecc1841bc1c07edd7f
Glitch (arthur-is-dope) <none>
2018-04-30 23:43:34 +0000

    🙉🔫 Checkpoint ./app.js:1032099/1021

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ const Spark = require("sparkbots")
 Spark.start({
 prefix: "prefix-here",
 token: "token-here" })
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('token');
