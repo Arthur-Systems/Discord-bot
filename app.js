0018470c05d279f96fd2cbb92a640719955d212a
Glitch (arthur-is-dope) <none>
2018-05-01 23:32:07 +0000

    Exporting to  Red-Fang/Toaster/Discordbot

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Spark = require("sparkbots")
 Spark.start({
 prefix: "~",
-token: process.env.TOKEN })
+token: process.env.TOKEN,
+ignoreUpdate: ["0.1.0-beta.0"]})
 
 var ClientInjRe = require('./express.js')
--- commands/arthur.js
+++ commands/arthur.js
@@ -1,8 +1,9 @@
-var Spark = require("sparkbots")
-const Command = Spark.command("arthur")
-Command.addAlias("playisgay")
+var Spark = require('sparkbots')
+const Command = Spark.command('arthur')
+Command.addAlias('playisgay")
 Command.setLevel(0)
 Command.allowDms(true)
+Command.setDescription('The best command')
 Command.code = (client, message) => {
 
   message.channel.send('play is gay')
