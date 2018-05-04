f8f5ec4a63b98081497aa345bf6c9f644a846057
Glitch (arthur-is-dope) <none>
2018-05-04 15:40:46 -0700

    g

--- commands/1.js
+++ commands/1.js
@@ -1,1 +1,16 @@
-var Spark=require('sparkbots')
+var Spark = require("sparkbots")
+const Command = Spark.command("say")
+Command.setLevel(0)
+Command.allowDms(true)
+Command.setDescription('._.')
+
+Command.code = (client, message) => {
+  if (message.content === "///" )
+  message.delete() return
+  else
+  message.delete()
+  message.channel.send((message.author.username) + " says: " + (message.content.replace('$say ','')));
+  
+}
+
+module.exports = Command;
--- commands/die.js
+++ commands/die.js
@@ -2,14 +2,13 @@ var Spark = require("sparkbots")
 const Command = Spark.command("die")
 Command.setLevel(9)
 Command.allowDms(true)
-Command.setDescription('Kills the bot')
+Command.setDescription('Stops the bot')
 module.exports = Command;
 
 Command.code = (client, message) => {
 
-  message.channel.send(":ok_hand::skin-tone-1:  Killing the bot...")
+  message.channel.send( return ":ok_hand::skin-tone-1:  Killing the bot...")
         .then(() => {
             process.exit()
         })
-  
 }
