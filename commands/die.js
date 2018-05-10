10e03265b85f0a54623fdb4ae4de556d9fb39684
Glitch (arthur-is-dope) <none>
2018-05-10 02:09:34 +0000

    👕👱 Updated with Glitch

--- commands/die.js
+++ commands/die.js
@@ -2,17 +2,10 @@ var Spark = require("sparkbots")
 const Command = Spark.command("die")
 Command.setLevel(9)
 Command.allowDms(true)
-Command.setDescription('Stops the bot')
+Command.setDescription('Kills the bot')
 module.exports = Command;
 
 Command.code = (client, message) => {
-<<<<<<< HEAD
-
-  message.channel.send( return ":ok_hand::skin-tone-1:  Killing the bot...")
-        .then(() => {
-            process.exit()
-        })
-=======
   message.channel.send(die())
         .then(() => {
             process.exit()
@@ -21,5 +14,4 @@ Command.code = (client, message) => {
 }
 function die() {
     return (":boom: Killing the bot...");
->>>>>>> f7351c7fa6fb56dfae8025b66aa35600ebc3ac92
 }
